d9ae7cc0848d909aa497214395eac7e460c748dc
Nenad Blagovcanin <nenadblagovcanin2@gmail.com>
2024-02-28 23:56:06 +0100

    initial files

---
+++ JavierLook/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using JavierLook.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JavierLook.Data
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Booking> bookings { get; set; }
+
+    }
+}
---
+++ JavierLook/Models/Booking.cs
@@ -0,0 +1,12 @@
+﻿namespace JavierLook.Models
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public DateTime DateFrom { get; set; }
+        public DateTime DateTo { get; set; }
+        public string BookedBy { get; set; }
+        public string Status { get; set; }
+    }
+}
